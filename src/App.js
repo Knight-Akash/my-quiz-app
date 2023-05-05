1ac7161f01782f1036dd0ddb458845b872af4697
Akash Shah <akash@infocusp.com>
2023-05-05 21:26:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router ,Routes, Route } from "react-router-dom";
+import Quiz from './components/Quefirebase';
+import quiz from './components/QueAns';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Quiz />
+    // <Router>
+    //   <Routes>
+    //   {/* <Route
+    //             path="/quiz"
+    //             element={
+    //               <quiz/>
+    //             }
+    //           /> */}
+    //           <Route path="/" element={<Quiz />} />
+    //   </Routes>
+
+
+    // </Router>
   );
 }
 
